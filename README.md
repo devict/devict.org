c9d634f5b93f4b1e540373e95a18b3503499b295
Jacob Walker <jacob@tortugas-llc.com>
2013-06-03 07:09:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# upfrontwichita.com
+
+The web home for UpFront Wichita
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpfrontwichitaCom::Application.config.secret_token = '0262b640cdab337a8a03498a14129dfbeb1a3de5475d8c02a239e93af4f306198e3154d9b7c9ca508e3451740b2b90d5164f4c71bfdde031c2d22658149dd5da'
