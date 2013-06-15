a4fcd8be413679717d4a37b88d8e2911056b07e0
Matt Conrad <mattconrad@gmail.com>
2013-06-15 11:17:07 -0500

    Simple iframe IRC clients. See irc_controller.rb for remarks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 UpfrontwichitaCom::Application.routes.draw do
+
+  get "irc/index"
+
+  get "irc/alt"
+
+  get "irc/alt2"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+
+  match "/irc", :to => 'irc#index'
+
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
