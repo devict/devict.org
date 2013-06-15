cbef514df42982490a1cb70d0415ac4d0b1bc30d
Matthew M. Keeler <keelerm84@gmail.com>
2013-06-15 13:09:00 -0500

    Updated to use UpFront Twitter application

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.middleware.use OmniAuth::Builder do
-    provider :twitter, 'z33UOOpJmBW4OlWB5ZdP0A', 'rKWY41eV5B3yEBXbgsIPDO6wogWKuXzU6PrNWGwk0s'
+    provider :twitter, 'soACn7JBJX4hgD8DnCltLg', 'iUALjJr5pgizorBJ3HoII0wN6iLyozmiSdis3QPIHA'
     provider :github, 'aba62c5941e052569974', '09990402d3f1df0513ad2ab949c56530539a7df9'
 end
