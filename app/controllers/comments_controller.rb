fe4afbf2be937f65eefe569b7ba4b24400b2ef7c
Matt Conrad <mattconrad@gmail.com>
2013-07-01 15:43:15 -0500

    Cleanup and improvement of comments on submissions.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,22 +2,12 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :owns_comment, :only => [:destroy, :edit, :update]
 
-  #MWCTODOs:
-  # edit form has bad stuff like Show on it that needs removed.
-  # need at least a little styling to separate submissions/comments, currently it's almost unreadable.
-  # not too sure about making submission_id attr_accessible. could avoid with a bit extra code.
-  # delete is being weird and not redirecting where it's supposed to.
-  # error scenarios need handled and tested.
-  # probably ought to test owns_comment with hacked edit (remove :edit above from before_filter to do this) 
-  # rm the show/new erbs.
-
   # decided to keep this, might be handy as an admin tool someday.
   def index
     @comments = Comment.all
 
     respond_to do |format|
       format.html # index.html.erb
-      #format.json { render json: @comments }
     end
   end
 
@@ -35,9 +25,11 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to challenge_submissions_path(:challenge_id => @comment.submission.challenge.id), notice: 'Comment was successfully created.' }
+        format.html { redirect_to challenge_submissions_path(:challenge_id => @comment.submission.challenge.id), notice: 'Comment added.' }
       else
-        format.html { render action: "new" }
+        # ideally we'd push back @comment with actual errors. not sure possible/advisable thru redirect.
+        flash[:error] = 'Your comment could not be saved. Comments may not be empty.'
+        format.html { redirect_to challenge_submissions_path(:challenge_id => @comment.submission.challenge.id) }
       end
     end
   end
@@ -48,7 +40,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.update_attributes(params[:comment])
-        format.html { redirect_to challenge_submissions_path(:challenge_id => @comment.submission.challenge.id), notice: 'Comment was successfully updated.' }
+        format.html { redirect_to challenge_submissions_path(:challenge_id => @comment.submission.challenge.id), notice: 'Comment was revised.' }
       else
         format.html { render action: "edit" }
       end
@@ -59,19 +51,18 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @challenge_id = @comment.submission.challenge.id
+
     @comment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to challenge_submissions_path(:challenge_id => @challenge_id), notice: 'Comment was removed.' }
-    end
+    redirect_to challenge_submissions_path(:challenge_id => @challenge_id), notice: 'Comment was removed.'
   end
 
   def owns_comment
     @comment = Comment.find(params[:id])
 
     if current_user != @comment.user
-      flash[:error] = 'You cannot modify a comment you did not create'
-      redirect_to submission_path redirect_to challenge_submissions_path(:challenge_id => @comment.submission.challenge.id)
+      flash[:error] = 'You cannot modify a comment you did not create.'
+      redirect_to challenge_submissions_path(:challenge_id => @comment.submission.challenge.id)
     end
   end
 end
