5d0430e5df85f213f3974fcc6a92308eb9ca194f
Matt Conrad <mattconrad@gmail.com>
2013-07-01 15:55:49 -0500

    Remove testing code, tweak comment about comment model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :submission
   belongs_to :user
 
-  # is this a good idea with :submission_id? it is for creates, but perhaps mischief with edits.
+  # is this a good idea with :submission_id? i don't think it allows anything "new" in terms of mischief.
   attr_accessible :body, :submission_id
 
   validates :body, :presence => true
