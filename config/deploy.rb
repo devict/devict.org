fad94ea86d97173947c0eae068ca4a0af1167470
Seth Etter <sethetter@gmail.com>
2013-12-03 14:59:07 -0600

    Update deploy target to new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,9 +13,9 @@ set :use_sudo, false
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "192.241.223.192"                          # Your HTTP server, Apache/etc
-role :app, "192.241.223.192"                          # This may be the same as your `Web` server
-role :db,  "192.241.223.192", :primary => true # This is where Rails migrations will run
+role :web, "162.23.147.78"                          # Your HTTP server, Apache/etc
+role :app, "162.23.147.78"                          # This may be the same as your `Web` server
+role :db,  "162.23.147.78", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup", "deploy:update_crontab"
