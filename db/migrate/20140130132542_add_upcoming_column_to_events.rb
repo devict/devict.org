7299fb05dd9c19d2636d873444d1b85b83be44c9
Seth Etter <sethetter@gmail.com>
2014-01-30 07:57:11 -0600

    Woops

--- db/migrate/20140130132542_add_upcoming_column_to_events.rb
+++ db/migrate/20140130132542_add_upcoming_column_to_events.rb
@@ -1,5 +1,5 @@
 class AddUpcomingColumnToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :upcoming, :boolean, default: 0
+    add_column :events, :upcoming, :boolean, default: false
   end
 end
