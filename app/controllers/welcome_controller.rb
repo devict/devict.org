f27539af36bce4a1e27e01483c41e737f761e394
Seth Etter <sethetter@gmail.com>
2014-01-30 08:12:51 -0600

    Featured event should order by date ascending

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @irc_users = IrcUser.order('handle')
     @events = Event.where(upcoming: false).order('date DESC').limit(4)
-    @featured_event = Event.where(upcoming: true).order('date DESC').first
+    @featured_event = Event.where(upcoming: true).order('date ASC').first
   end
 end
