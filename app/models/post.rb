17c653ae12926edaeb9a609cd412213ca39a8845
Matthew M. Keeler <keelerm84@gmail.com>
2014-10-19 00:00:54 -0400

    Continue updating posts if one fails

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,19 +8,24 @@ class Post < ActiveRecord::Base
 
     @feeds.each do |feed|
       puts feed.url
-      feed_handle = Feedzirra::Feed.fetch_and_parse(feed.url)
 
-      feed_handle.entries.each do |entry|
-        unless Post.exists? :guid => entry.entry_id
-          Post.create!(
-                       :title        => entry.title,
-                       :summary      => entry.summary,
-                       :url          => entry.url,
-                       :published_at => entry.published,
-                       :guid         => entry.entry_id,
-                       :feed         => feed
-                       )
-        end
+      begin
+          feed_handle = Feedzirra::Feed.fetch_and_parse(feed.url)
+
+          feed_handle.entries.each do |entry|
+            unless Post.exists? :guid => entry.entry_id
+              Post.create!(
+                           :title        => entry.title,
+                           :summary      => entry.summary,
+                           :url          => entry.url,
+                           :published_at => entry.published,
+                           :guid         => entry.entry_id,
+                           :feed         => feed
+                           )
+            end
+          end
+      rescue
+          # Continue
       end
     end
   end
