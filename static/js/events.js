bb99dcf0cb28f0db0f2c7bd8b1ce7490a997c0ca
Sandip Southekal <sandip@ziprecruiter.com>
2016-10-05 21:23:47 -0500

    setting html content instead of building the entire html

--- static/js/events.js
+++ static/js/events.js
@@ -1,27 +1,25 @@
 $(function(){
+	var results = $("#results");
+	results.empty();
     $.ajax({
         url : 'https://devict-patreon.herokuapp.com/events',
         type: 'GET',
         success : function(data) {
-			var htmlString = "";
 			$.each(data.results, function (i, item) {
-	    		htmlString += '<div class="panel panel-default"> \
-	    							<div class="panel-heading">  \
-	    								<h3 class="panel-title"> \
-	    									<a href="' + item.event_url + '" target="_blank" style="text-transform:uppercase">' + item.name + '</a> \
-										</h3> \
-									</div> \
-									<div class="panel-body"> \
-										<p>' + item.description + '</p> \
-										<b>Venue:</b> ' + item.venue.name + ', ' + item.venue.address_1 + ', ' + item.venue.city + '&nbsp;  \
-										<a href="https://www.google.com/maps/@' + item.venue.lat + ',' + item.venue.lon + ',18z" target="_blank"> <i class="fa fa-map-marker" aria-hidden="true"></i> Map It!</a> \
-										<p><b>Date and Time</b>: ' + epochConv(item.time) + '</p> \
-										<p><a href=' + item.event_url + ' data-event=' + item.id + ' class="mu-rsvp-btn">RSVP</a> &nbsp; \
-										<a class="btn btn-sm btn-default" href="' + item.event_url + '" target="_blank" role="button"><i class="fa fa-external-link" aria-hidden="true"></i> Go to Meetup</a><br></p> \
-									</div> \
-								</div>';
+	    		var t = $("#events div").clone();
+	    		t.find(".event_url").attr("href", item.event_url);
+	    		t.find(".event_name").text(item.name);
+	    		t.find(".event_description").html(item.description);
+		        t.find(".event_venue_name").text(item.venue.name);
+		        t.find(".event_venue_address").text(item.venue.address_1);
+				t.find(".event_venue_city").text(item.venue.city);
+				t.find(".event_map").attr("href", 'https://www.google.com/maps/@' + item.venue.lat + ',' + item.venue.lon + ',18z');
+				t.find(".event_time").text(epochConv(item.time));
+				results.append(t[0]);
 			});
-			$('#events').html(htmlString);
+
+			$("#loading").addClass("hidden");
+    		$("#results").removeClass("hidden");
     	}
     })  
 })
