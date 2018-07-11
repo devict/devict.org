06ad30648799829cf8157b3f346e407f9dafa1e1
Kenton Hansen <kenton@roll20.net>
2018-07-10 21:43:30 -0500

    Adds employer link to modal

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 (function() {
@@ -6,13 +7,6 @@
 
     function openModalWindow(e) {
         var child = document.createElement('div');
-<<<<<<< HEAD
-            child.className = "inside";
-        var text = document.createElement('h1');
-            text.className = "inside-text";
-            text.innerHTML = "2018 devICT Salary Survey</h1><p>As part of an effort to better understand the software development ecosystem in Wichita, devICT is conducting a salary survey.</p><h2><a class=modal-link target=_blank href=https://kentonh.typeform.com/to/SNdj46>Please Participate >>>></a>";
-        child.appendChild(text);
-=======
         child.className = "inside";
         child.innerHTML = `
             <h1 class="inside-text">2018 devICT Salary Survey</h1>
@@ -24,11 +18,12 @@
             </p>
             <h2>
                 <a class="btn btn-lg btn-success modal-link" target="_blank" href="https://kentonh.typeform.com/to/SNdj46">
-                    Please Participate >>>>
+                    Individuals Participate >>>>
                 </a>
+                <a class="btn btn-lg btn-success modal-link" target="_blank" href="https://form.jotform.com/81908941084160">Employers Participate >>>></a>
+
             </h2>
         `;
->>>>>>> d1e9d2c6af74f60e1ad9398332631fd7c34f1b37
 
         //create modal instance and pass in child elements
             //can be whatever, styled however you want
