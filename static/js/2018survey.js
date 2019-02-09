459454c32dad112c51bb51de48b00e7f89bb6cba
Kenton Hansen <kenton@roll20.net>
2019-02-09 09:33:58 -0600

    Adds charts to Developer Tab

--- static/js/2018survey.js
+++ static/js/2018survey.js
@@ -54,6 +54,7 @@ var lineChart = new Chart(ctx, {
   }
 });
 
+// All Respondents Percentile Year
 var ctx = document.getElementById("percentileChart").getContext("2d");
 
 var data = {
@@ -187,6 +188,131 @@ var lineChart = new Chart(ctx, {
           callback: function(value, index, values) { return '$' + value; },
         }
       }],
+    }
+  }
+});
+
+// Developer Percentile Year
+var ctx = document.getElementById("DevPercentileChart").getContext("2d");
+
+var data = {
+  labels: ["", "25th Percentile", "50th Percentile", "75th Percentile", "90th Percentile", ""],
+  datasets: [{
+    label: "2017",
+    backgroundColor: 'rgba(255,99,132,0.4)',
+    borderColor: 'rgba(255,99,132,1)',
+    borderWidth: 3,
+    data: [, 53250.00, 68150.00, 80272.25, 99200.00, ]
+    // Average value 68942.97,
+  }, {
+    label: "2017 Avearge",
+    // backgroundColor: "rgba(200,200,200,0.6)",
+    borderColor: 'rgba(100,100,100,0.8)',
+    borderWidth: 3,
+    data: [68942.97, 68942.97, 68942.97, 68942.97, 68942.97, 68942.97]
+  }, {
+    label: "2018",
+    backgroundColor: "rgba(54,162,235,0.4)",
+    borderColor: "'rgba(54,162,235,1)'",
+    borderWidth: 3,
+    data: [, 52000.00, 68455.00, 87875.50, 99800.00, ]
+  }, {
+    label: "2018 Avearge",
+    // backgroundColor: "rgba(200,200,200,0.6)",
+    borderColor: "'rgba(200,200,200,0.6)'",
+    borderWidth: 3,
+    data: [69764.50, 69764.50, 69764.50, 69764.50, 69764.50, 69764.50]
+  }]
+};
+
+var lineChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: {
+    categorySpacing: 20,
+    spanGaps: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 0,
+          callback: function(value, index, values) {
+              value = value.toString();
+              value = value.split(/(?=(?:...)*$)/);
+              value = value.join(',');
+              return value;
+              return '$' + value;
+            },
+        }
+      }]
+    }
+  }
+});
+
+
+// Developer Percentile Year
+var ctx = document.getElementById("DevPercentilesBYExp").getContext("2d");
+
+var data = {
+  labels: ["25th Percentile", "50th Percentile", "75th Percentile", "90th Percentile"],
+  datasets: [{
+    label: "2017 1-4",
+    backgroundColor: 'rgba(255,99,132,0.1)',
+    borderColor: 'rgba(255,99,132,1)',
+    borderWidth: 3,
+    data: [50000.00, 60000, 60000, 60000]
+    // Average value 68942.97,
+  }, {
+    label: "2017 5-10",
+     backgroundColor: "rgba(255,99,132,0.1)",
+    borderColor: 'rgba(255,99,132,1)',
+    borderWidth: 3,
+    data: [57750, 64400, 78500, 80108.9]
+  }, {
+    label: "2017 11+",
+     backgroundColor: "rgba(255,99,132,0.1)",
+    borderColor: 'rgba(255,99,132,1)',
+    borderWidth: 3,
+    data: [78500, 88166.5, 101000, 117500]
+  }, {
+    label: "2018 1-4",
+    backgroundColor: "rgba(54,162,235,0.1)",
+    borderColor: "'rgba(54,162,235,1)'",
+    borderWidth: 3,
+    data: [41875, 56000, 63852.5, 91031.0]
+  }, {
+    label: "2018 5-10",
+     backgroundColor: "rgba(54,162,235,0.1)",
+    borderColor: "'rgba(54,162,235,1)'",
+    borderWidth: 3,
+    data: [60315, 65750, 82921.5, 87400]
+  }, {
+    label: "2018 5-10",
+     backgroundColor: "rgba(54,162,235,0.1)",
+    borderColor: "'rgba(54,162,235,1)'",
+    borderWidth: 3,
+    data: [78750, 94251, 101000, 117500]
+  }]
+};
+
+var lineChart = new Chart(ctx, {
+  type: 'bar',
+  data: data,
+  options: {
+    categorySpacing: 20,
+    spanGaps: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 0,
+          callback: function(value, index, values) {
+              value = value.toString();
+              value = value.split(/(?=(?:...)*$)/);
+              value = value.join(',');
+              return value;
+              return '$' + value;
+            },
+        }
+      }]
     }
   }
 });
